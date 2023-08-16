d3a28f3bbdbd4b56079b097a7dd1925775aa7195
Cynthia-mbesa <cynthiambesa@gmail.com>
2023-08-17 02:07:17 +0300

    2-uniq_add.py

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    """
+    a function that adds all unique integers in a
+    list (only once for each integer)
+    """
+    unique_sum = sum(set(my_list))
+    return unique_sum
