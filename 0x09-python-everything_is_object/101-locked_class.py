179dae92eb95aab9406a3444a886bdfe17451959
Cynthia-mbesa <cynthiambesa@gmail.com>
2023-09-07 03:32:21 +0300

    Everything is object

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""101-locked_class - defines a class LockedClass"""
+
+
 class LockedClass:
+    """class representing LockedClass"""
     def __setattr__(sel, name, value):
         if name == "first_name":
             super().__setattr__(name, value)
