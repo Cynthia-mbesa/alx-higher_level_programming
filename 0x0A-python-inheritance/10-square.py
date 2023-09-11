d7d59ac3e8c7f364d23a95f61761256f72552e20
Cynthia-mbesa <cynthiambesa@gmail.com>
2023-09-12 01:12:31 +0300

    Python - Inheritance

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -47,4 +47,4 @@ class Square(Rectangle):
         super().__init__(size, size)
 
     def __str__(self):
-        return f"[Square] {self._Rectangle__width}/{self._Rectangle__height}"
+        return f"[Rectangle] {self._Rectangle__width}/{self._Rectangle__height}"
