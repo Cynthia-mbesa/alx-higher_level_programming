191206b39eba20f2e0a267bf50cf1218d9d5f554
Cynthia-mbesa <cynthiambesa@gmail.com>
2023-09-13 01:03:24 +0300

    Input/Output

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""module containing student class"""
+
+
+class Student:
+    """defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """instantiates a student object"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns dictionary representation of student"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {key: self.__dict__.get(key) for key in attrs}
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the student instance
+        with values from the dictionary"""
+        for key, value in json.items():
+            setattr(self, key, value)
