ed212a4bc162d0b8b1a95b51c13bd6e70209d087
Cynthia-mbesa <cynthiambesa@gmail.com>
2023-12-02 01:19:35 +0300

    correction

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL, 
+"""Python script that takes in a URL,
 sends a request to the URL and displays the body of the response.
 
-If the HTTP status code is greater than or equal to 400, 
+If the HTTP status code is greater than or equal to 400,
 print: Error code: followed by the value of the HTTP status code
 You must use the packages requests and sys
 You are not allowed to import packages other than requests and sys
