604db1e878f7ac14cb3b3e4b542cf4dc44144ce7
Cynthia-mbesa <cynthiambesa@gmail.com>
2023-12-02 01:14:05 +0300

    correction

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Write a Python script that takes in a URL, 
-sends a request to the URL and 
+"""Write a Python script that takes in a URL,
+sends a request to the URL and
 displays the body of the response (decoded in utf-8).
 
-You have to manage urllib.error.HTTPError exceptions and 
+You have to manage urllib.error.HTTPError exceptions and
 print: Error code: followed by the HTTP status code
 You must use the packages urllib and sys
 You are not allowed to import other packages than urllib and sys
